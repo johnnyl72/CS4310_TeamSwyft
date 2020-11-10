ef71bb23b7d2502222974144197c124483b1f4d7
Johnny Lam <35053246+johnnyl72@users.noreply.github.com>
2020-11-10 02:57:02 -0800

    implement basic multithreading for server response processing

--- serversocket.c
+++ serversocket.c
@@ -5,27 +5,16 @@
 #include <unistd.h> // Unix standard
 #include <arpa/inet.h> // Integer to network representation
 #include <errno.h> // Error Message
-#include <stdarg.h> // for varargs
-#include <sys/types.h>
-#include <signal.h>
-#include <fcntl.h> 
-#include <sys/time.h>
-#include <sys/ioctl.h>
-#include <netdb.h>
-
+#include <pthread.h>
 #define SERVER_PORT 1234
 #define MAX_BUFF_SIZE 4096
 
-void err_n_die(const char *fmt, ...);
-char *bin2hex(const unsigned char *input, size_t len);
+void * process(void* connect_socket);
 
 int main(int argc, char **argv){
 
-    int listen_socket, connect_socket, n;
+    int listen_socket, connect_socket;
     struct sockaddr_in server_address;
-    uint8_t buff[MAX_BUFF_SIZE];
-    uint8_t recvline[MAX_BUFF_SIZE];
-
     // Create socket
     if ( (listen_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0 ){
         perror("Socket Creation Failed!");
@@ -41,11 +30,11 @@ int main(int argc, char **argv){
     server_address.sin_port = htons(SERVER_PORT); // Arbitrary port number
 
     // Listen & Bind our listen_socket to our server_address (i.e. INADDR_ANY)
-    if ( (bind(listen_socket, (struct sockaddr *) &server_address, sizeof(server_address)) ) < 0 ) {
+    if ( (bind(listen_socket, (struct sockaddr *) &server_address, sizeof(server_address))) < 0 ) {
         perror("Binding Error!");
         exit(1);
     }
-    if ( (listen(listen_socket, 2) ) < 0){
+    if ( (listen(listen_socket, 2)) < 0 ){
         perror("Listen Error!");
         exit(1);
     }
@@ -60,28 +49,42 @@ int main(int argc, char **argv){
         fflush(stdout); // Force flush mainly for debugging purposes: https://www.youtube.com/watch?v=T9adOz_EHnQ
 
         // connect_socket is another socket that is used to talk with the connected client
-        connect_socket = accept(listen_socket, (struct sockaddr *) &client_address, &addr_len); // pass into listening file descriptor
+        if ( (connect_socket = accept(listen_socket, (struct sockaddr *) &client_address, &addr_len)) < 0) { // pass into listening file descriptor
+            perror("Accept failed!");
+            exit(1);
+        } 
 
         // Print the client's ip address 
         inet_ntop(AF_INET, &client_address, client_ip, MAX_BUFF_SIZE); // Network to presentation format (ntop) due to client_address being a struct (network type) full of binary data
-        printf("Client's IP address: %s\n", client_ip);
+        printf("Client's IP address: %s\n", client_ip); // Bug here on first connection
 
-        // Read client's message
-        memset(recvline, '\0', MAX_BUFF_SIZE);  // Zero out the receive buffer to make sure it ends up null-terminated
-
-        while ((n = read(connect_socket, recvline, MAX_BUFF_SIZE)) > 0){
+        // Process client's message
+        pthread_t thread;
+        // process(connect_socket);
+        pthread_create(&thread, NULL, process, &connect_socket);
+        
+    }
+    return 0;
+}
+void * process(void* p_connect_socket){
+    int connect_socket = *( (int*) p_connect_socket);
+    int message_size;
+    char buff[MAX_BUFF_SIZE], recvline[MAX_BUFF_SIZE];
+    sleep(10);
+    memset(recvline, '\0', MAX_BUFF_SIZE);  // Zero out the receive buffer to make sure it ends up null-terminated
+    while ((message_size = read(connect_socket, recvline, MAX_BUFF_SIZE)) > 0){ // read(): https://pubs.opengroup.org/onlinepubs/009695399/functions/read.html
 
             // printf vs sprtinf vs fprintf: https://www.geeksforgeeks.org/difference-printf-sprintf-fprintf/
             printf("\n%s\n", recvline);
 
-            if (recvline[n-1] == '\n'){
+            if (recvline[message_size-1] == '\n'){
                 break;
             }
 
             memset(recvline, 0, MAX_BUFF_SIZE); // Zero out to read next time so we know its null-terminated
         }
 
-        if (n < 0){
+        if (message_size < 0){
             perror("There was a reading error!");
             exit(1);
         }
@@ -90,8 +93,9 @@ int main(int argc, char **argv){
         snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK\r\n\r\nHello"); // Our next job is to create a method to create a proper HTTP response back to display our game
         // snprintf((char*)buff, sizeof(buff), "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!");
 
-        write(connect_socket, (char*)buff, strlen((char*)buff)); // Write the response (eventually our web page) into this socket
+        write(connect_socket, buff, strlen((char*)buff)); // Write the response (eventually our web page) into this socket
         close(connect_socket);
-    }
-    return 0;
+        printf("End of connection");
+
+        // return NULL;
 }
